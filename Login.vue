2155420ee7dee5c64c075fb495dba84b85a7f365
龚传俊 <51748837+fiafeng@users.noreply.github.com>
2026-04-24 20:58:53 +0800

    Add Login.vue to master

---
+++ Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="login-container">
+    <h1>Login</h1>
+    <input type="text" v-model="username" placeholder="Username" />
+    <input type="password" v-model="password" placeholder="Password" />
+    <button @click="login">Login</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginPage',
+  data() {
+    return { username: '', password: '' }
+  },
+  methods: {
+    login() {
+      // TODO: implement login
+    }
+  }
+}
+</script>
